d959b8e07b406ff974fecd489914e347b7484d43
juny <king932z@naver.com>
2023-09-06 23:19:24 +0900

    spring-boot-blog

--- src/main/java/jjay/blog/developer/SpringBootDeveloperApplication.java
+++ src/main/java/jjay/blog/developer/SpringBootDeveloperApplication.java
@@ -2,7 +2,9 @@ package jjay.blog.developer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
--- src/main/java/jjay/blog/developer/domain/Article.java
+++ src/main/java/jjay/blog/developer/domain/Article.java
@@ -1,36 +1,50 @@
 package jjay.blog.developer.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@NoArgsConstructor
-@Entity //엔티티 지정
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
-    @Id //ID가 기본키
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키는 자동으로 1씩 증가
-    @Column(name="id", updatable = false)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
     private Long id;
 
-
-    @Column(name = "title", nullable = false) //title not null 처리
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name ="content", nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
-    
-    @Builder // 빌더 패턴으로 객체 생성
-    public Article(String title, String content){
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    @Builder
+    public Article(String title, String content) {
         this.title = title;
         this.content = content;
     }
-
 
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
-
 }
